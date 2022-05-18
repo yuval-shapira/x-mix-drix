f0c5cba40e7c1190a07dddffc09dad17644a3a24
Yuval Shapira <uv1202@gmail.com>
2022-05-18 10:32:37 +0300

    Create new project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <h1>X Mix Drix</h1>
+      </header>
+      <main className="table-container"></main>
+    </>
+  );
+}
+
+export default App;
