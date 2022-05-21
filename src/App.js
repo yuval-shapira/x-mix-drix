7cffbb7639d3c6ff212e15ee3f788c447eda473f
Yuval Shapira <uv1202@gmail.com>
2022-05-21 22:36:40 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import React from "react";
 import "./App.css";
 
+function TableComponant(props) {
+  const cell = props.cell;
+  return <div className={cell}>{cell}</div>;
+}
+
 function App() {
+  const tableGame = [1, 2, 3, 4, 5, 6, 7, 8, 1545454];
   return (
     <>
       <header className="App-header">
         <h1>X Mix Drix</h1>
       </header>
-      <main className="table-container"></main>
+      <main className="table-container">
+        <div className="grid-container">
+          {tableGame.map((cell) => (
+            <TableComponant key={cell} cell={cell} />
+          ))}
+        </div>
+      </main>
     </>
   );
 }
+
 
 export default App;
